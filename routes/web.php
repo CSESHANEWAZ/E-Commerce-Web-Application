b7f807744b911cbde7477bb5d907df48536fd5af
cseshanewaz <mdshanewazakib027@gmail.com>
2021-07-21 01:26:29 +0600

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\AboutComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +21,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class);
+
+Route::get('/about', AboutComponent::class);
+
+Route::get('/shop', ShopComponent::class);
+
+Route::get('/cart', CartComponent::class);
+
+Route::get('/checkout', CheckoutComponent::class);
+
+Route::get('/contact', ContactComponent::class);
+
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+
+//for user or customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+     Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+//for admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
+     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
 });
