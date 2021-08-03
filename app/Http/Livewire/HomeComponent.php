2939f1cdd0ed89661c824c5ba4b3499851dcfce0
cseshanewaz <mdshanewazakib027@gmail.com>
2021-08-04 02:08:17 +0600

    commit

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -4,12 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\HomeSlider;
+use App\Models\Product;
 
 class HomeComponent extends Component
 {
     public function render()
     {
         $sliders = HomeSlider::where('status', 1)->get();
-        return view('livewire.home-component', ['sliders'=>$sliders])->layout('layouts.base');
+        $latest_products = Product::orderBy('created_at', 'DESC')->get()->take(8);
+
+        return view('livewire.home-component', ['sliders'=>$sliders, 'latest_products'=>$latest_products])->layout('layouts.base');
     }
 }
